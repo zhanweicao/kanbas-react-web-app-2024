bf37b2c19d8754b6771566369c2c8edfa0fd69c3
zhanweicao <chanweicao@gmail.com>
2024-07-03 10:48:15 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import Kanbas from './Kanbas';
+
+
+function App() {
+  return (
+      <HashRouter>
+          <div>
+              <Routes>
+                  <Route path="/" element={<Navigate to="Labs" />} />
+                  <Route path="/Labs/*" element={<Labs />} />
+                  <Route path="/Kanbas/*" element={<Kanbas />} />
+              </Routes>
+          </div>
+      </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,6 @@
+export default function Kanbas() {
+    return (
+        <div id="wd-kanbas">
+            <h1>Kanbas</h1>
+        </div>
+    );}
