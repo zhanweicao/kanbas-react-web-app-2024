bdddf3e984f9faf618a03dd98e833986e2ab4aed
zhanweicao <chanweicao@gmail.com>
2024-07-14 13:19:05 +0800

    4.6 DONE

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,15 +1,58 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle, FaBullhorn } from "react-icons/fa";
+import { BiImport, BiHomeCircle } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { MdStream } from "react-icons/md";
+import { HiOutlineChartBar } from "react-icons/hi";
+
 export default function CourseStatus() {
-    return (
-      <div id="wd-course-status">
-        <h2>Course Status</h2>
-        <button>Unpublish</button> <button>Publish</button><br />
-        <button>import Existing Content</button><br />
-        <button>import from Commomns</button><br />
-        <button>Choose Home Page</button><br />
-        <button>View Course Stream</button><br />
-        <button>New announcement</button><br />
-        <button>New Analytics</button><br />
-        <button>View Course Notifications</button>
+  return (
+    <div id="wd-course-status" style={{ width: "300px" }}>
+      <h2>Course Status</h2>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <button className="btn btn-lg btn-secondary w-100 text-nowrap">
+            <MdDoNotDisturbAlt className="me-2 fs-5" />
+            Unpublish
+          </button>
+        </div>
+        <div className="w-50">
+          <button className="btn btn-lg btn-success w-100">
+            <FaCheckCircle className="me-2 fs-5" />
+            Publish
+          </button>
+        </div>
       </div>
-  );}
-  
+      <br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" />
+        Import Existing Content
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" />
+        Import from Commons
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiHomeCircle className="me-2 fs-5" />
+        Choose Home Page
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <MdStream className="me-2 fs-5" />
+        View Course Stream
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <FaBullhorn className="me-2 fs-5" />
+        New Announcement
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <HiOutlineChartBar className="me-2 fs-5" />
+        New Analytics
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <IoMdNotificationsOutline className="me-2 fs-5" />
+        View Course Notifications
+      </button>
+    </div>
+  );
+}
