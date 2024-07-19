0d4e2790f241a9eb30c286c7ea44b175de030bd8
zhanweicao <chanweicao@gmail.com>
2024-07-19 16:19:51 +0800

    3.4 {course && course.name} NOT WORKING 3.5 DONE

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,17 +1,22 @@
+import { courses } from "../Database";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Grades from "./Grades";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useLocation, useParams } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234
+        {/* Test */}
+        {course && course.name} &gt; {pathname.split("/")[4]}
       </h2>
       <hr />
       <CoursesNavigation />
--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,215 +1,34 @@
+import { Link } from "react-router-dom";
+import * as db from "../Database";
 export default function Dashboard() {
-    return (
-        <div id="wd-dashboard">
-            <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-            <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
-            <div id="wd-dashboard-courses" className="row">
-                <div className="row row-cols-1 row-cols-md-5 g-4">
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/1234/Home">
-                                <img src="/images/react.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS1234 React JS
-                                    </h5>
-                                    <p className="card-text">
-                                        Full Stack software developer
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/2234/Home">
-                                <img src="/images/python.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS2234 Python
-                                    </h5>
-                                    <p className="card-text">
-                                        Introduce to Programming Language
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/3234/Home">
-                                <img src="/images/java.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS3234 Java
-                                    </h5>
-                                    <p className="card-text">
-                                        OOD Programming
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/4234/Home">
-                                <img src="/images/cpp.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS4234 CPP
-                                    </h5>
-                                    <p className="card-text">
-                                        CPP Programming
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/5234/Home">
-                                <img src="/images/sql.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5234 Database
-                                    </h5>
-                                    <p className="card-text">
-                                        Introduce to Database Management
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/6234/Home">
-                                <img src="/images/rstudio.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS6234 R Studio
-                                    </h5>
-                                    <p className="card-text">
-                                        Data Analysis
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card">
-                            <a className="wd-dashboard-course-link text-decoration-none text-dark"
-                                href="#/Kanbas/Courses/7234/Home">
-                                <img src="/images/matlab.jpg" width="100%" />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS7234 Matlab
-                                    </h5>
-                                    <p className="card-text">
-                                        Matlab in Engineering
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+  const courses = db.courses;
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          {courses.map((course) => (
+            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+              <Link to={`/Kanbas/Courses/${course._id}/Home`} className="text-decoration-none" >
+                <div className="card rounded-3 overflow-hidden">
+                  <img src="/images/react.jpg" height="{160}" />
+                  <div className="card-body">
+                    <span className="wd-dashboard-course-link"
+                      style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }} >
+                      {course.name}
+                    </span>
+                    <p className="wd-dashboard-course-title card-text" style={{ maxHeight: 53, overflow: "hidden" }}>
+                      {course.description}
+                    </p>
+                    <Link to={`#/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                  </div>
                 </div>
+              </Link>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-
-
-{/* <div className="wd-dashboard-course">
-                <img src="/images/java.jpg" width={200} />
-                    <div>
-                        <a className="wd-dashboard-course-link"
-                           href="#/Kanbas/Courses/1234/Home">
-                            CS5004
-                        </a>
-                        <p className="wd-dashboard-course-title">
-                            Object-Oriented Design
-                        </p>
-                        <a href="https://www.java.com/en/"> Go </a>
-                    </div>
-                </div>
-                <div className="wd-dashboard-course">
-                <img src="/images/python.jpg" width={200} />
-                    <div>
-                        <a className="wd-dashboard-course-link"
-                           href="#/Kanbas/Courses/1234/Home">
-                            CS5001
-                        </a>
-                        <p className="wd-dashboard-course-title">
-                            Intensive Foundations of CS
-                        </p>
-                        <a href="https://www.java.com/en/"> Go </a>
-                    </div>
-                </div>
-                <div className="wd-dashboard-course">
-                <img src="/images/math.jpg" width={200} />
-                    <div>
-                        <a className="wd-dashboard-course-link"
-                           href="#/Kanbas/Courses/1234/Home">
-                            CS5002
-                        </a>
-                        <p className="wd-dashboard-course-title">
-                            Discrete Structure
-                        </p>
-                        <a href="https://www.java.com/en/"> Go </a>
-                    </div>
-                </div>
-                <div className="wd-dashboard-course">
-                <img src="/images/cpp.jpg" width={200} />
-                    <div>
-                        <a className="wd-dashboard-course-link"
-                           href="#/Kanbas/Courses/1234/Home">
-                            CS5008
-                        </a>
-                        <p className="wd-dashboard-course-title">
-                        Data Structures, Algorithms, and Their Applications Within Computer Systems
-                        </p>
-                        <a href="https://www.java.com/en/"> Go </a>
-                    </div>
-                </div>
-                <div className="wd-dashboard-course">
-                <img src="/images/sql.jpg" width={200} />
-                    <div>
-                        <a className="wd-dashboard-course-link"
-                           href="#/Kanbas/Courses/1234/Home">
-                            CS5200
-                        </a>
-                        <p className="wd-dashboard-course-title">
-                            Database Management
-                        </p>
-                        <a href="https://www.java.com/en/"> Go </a>
-                    </div>
-                </div>
-                <div className="wd-dashboard-course">
-                <img src="/images/java.jpg" width={200} />
-                    <div>
-                        <a className="wd-dashboard-course-link"
-                           href="#/Kanbas/Courses/1234/Home">
-                            CS5004 OOD programming
-                        </a>
-                        <p className="wd-dashboard-course-title">
-                            Object-Oriented Design
-                        </p>
-                        <a href="https://www.java.com/en/"> Go </a>
-                    </div>
-                </div> */}
-
-
-
