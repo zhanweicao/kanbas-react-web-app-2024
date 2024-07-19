16c22028cc4e5f937ad92f114fdd620ddfac7d1a
zhanweicao <chanweicao@gmail.com>
2024-07-19 19:18:30 +0800

    3.6 DONE. 3.4 DOES NOT WORK

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,14 +1,69 @@
+import { courses } from "../../Database";
+import { useParams } from "react-router";
 import "./index.css";
 export default function CoursesNavigation() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const links = [
+    "Home",
+    "Modules",
+    "Piazza",
+    "Zoom",
+    "Assignments",
+    "Quizzes",
+    "Grades",
+  ];
   return (
-    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
-      <a id="wd-course-home-link" href="#/Kanbas/Courses/1234/Home" className="list-group-item active border border-0">Home</a>
-      <a id="wd-course-modules-link" href="#/Kanbas/Courses/1234/Modules" className="list-group-item text-danger border border-0">Modules</a>
-      <a id="wd-course-piazza-link" href="#/Kanbas/Courses/1234/Piazza" className="list-group-item text-danger border border-0">Piazza</a>
-      <a id="wd-course-zoom-link" href="#/Kanbas/Courses/1234/Zoom" className="list-group-item text-danger border border-0">Zoom</a>
-      <a id="wd-course-quizzes-link" href="#/Kanbas/Courses/1234/Assignments" className="list-group-item text-danger border border-0">Assignments</a>
-      <a id="wd-course-assignments-link" href="#/Kanbas/Courses/1234/Quizzes" className="list-group-item text-danger border border-0">Quizzes</a>
-      <a id="wd-course-grades-link" href="#/Kanbas/Courses/1234/Grades" className="list-group-item text-danger border border-0">Grades</a>
+    <div id="wd-courses-navigation" className="list-group  rounded-0">
+      <a
+        id="wd-course-home-link"
+        href={`#/Kanbas/Courses/${course?._id}/Home`}
+        className="list-group-item border border-0 active"
+      >
+        Home
+      </a>
+      <a
+        id="wd-course-modules-link"
+        href={`#/Kanbas/Courses/${course?._id}/Modules`}
+        className="list-group-item text-danger border border-0"
+      >
+        Modules
+      </a>
+      <a
+        id="wd-course-piazza-link"
+        href="#/Kanbas/Courses/1234/Piazza"
+        className="list-group-item text-danger border border-0"
+      >
+        Piazza
+      </a>
+      <a
+        id="wd-course-zoom-link"
+        href="#/Kanbas/Courses/1234/Zoom"
+        className="list-group-item text-danger border border-0"
+      >
+        Zoom
+      </a>
+      <a
+        id="wd-course-quizzes-link"
+        href={`#/Kanbas/Courses/${course?._id}/Assignments`}
+        className="list-group-item text-danger border border-0"
+      >
+        Assignments
+      </a>
+      <a
+        id="wd-course-assignments-link"
+        href="#/Kanbas/Courses/1234/Quizzes"
+        className="list-group-item text-danger border border-0"
+      >
+        Quizzes
+      </a>
+      <a
+        id="wd-course-grades-link"
+        href="#/Kanbas/Courses/1234/Grades"
+        className="list-group-item text-danger border border-0"
+      >
+        Grades
+      </a>
     </div>
   );
 }
