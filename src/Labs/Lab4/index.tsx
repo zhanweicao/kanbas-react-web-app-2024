f0f4e4ff8b550fca6a9db9fd6aa6404b58e0057c
zhanweicao <chanweicao@gmail.com>
2024-07-27 15:45:07 -0700

    2.3.8 Done

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,14 @@
+import ArrayStateVariable from "./ArrayStateVaribales";
+import BooleanStateVariables from "./BooleanStateVariables";
 import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
     function sayHello() {
@@ -12,10 +19,17 @@ export default function Lab4() {
     return (
         <div id="wd-a4">
             <h2>Lab 4</h2>
+            <ObjectStateVariable/>
+            <ArrayStateVariable/>
+            <ParentStateComponent/>
             <ClickEvent />
             <PassingDataOnEvent/>
             <PassingFunctions theFunction={sayHello} />
             <EventObject/>
+            <Counter/>
+            <BooleanStateVariables/>
+            <StringStateVariables/>
+            <DateStateVariable/>
         </div>
     )
 }
