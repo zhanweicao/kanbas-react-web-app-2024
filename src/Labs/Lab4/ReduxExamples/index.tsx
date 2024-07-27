512a6f95cb1339006105ae3c8ed9fdeb4653741d
zhanweicao <chanweicao@gmail.com>
2024-07-27 16:24:39 -0700

    2.5 DONE

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 
 export default function ReduxExamples() {
     return (
         <div>
             <h2>Redux Examples</h2>
+            <AddRedux />
             <CounterRedux />
             <HelloRedux />
         </div>
