e728eda47798fbdfaf82cf3bdcda11a09b3d1f73
zhanweicao <chanweicao@gmail.com>
2024-07-27 16:25:13 -0700

    2.4 DONE

--- src/Labs/store/index.tsx
+++ src/Labs/store/index.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
+import addReducer from "../Lab4/ReduxExamples/AddRedux/addReducer";
 const store = configureStore({
     reducer: {
         helloReducer,
         counterReducer,
+        addReducer,
     },
 });
 export default store;
