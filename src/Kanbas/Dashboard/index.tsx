3ccb366fbf75adfcc029ecb62ab1bb6c43187551
zhanweicao <chanweicao@gmail.com>
2024-07-27 23:09:43 -0700

    3.1.2 DONE

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,10 +1,38 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "../Database";
 export default function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/react.jpg", description: "New Description"
+  });
+  const addNewCourse = () => {
+    const newCourse = {
+      ...course,
+      _id: new Date().getTime().toString()
+    };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+        <button className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse} > Add </button>
+      </h5><br />
+      <input value={course.name} className="form-control mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+      <textarea value={course.description} className="form-control"
+        onChange={(e) => setCourse({ ...course, description: e.target.value })} /><hr />
+
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
@@ -22,6 +50,13 @@ export default function Dashboard() {
                       {course.description}
                     </p>
                     <Link to={`#/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                    <button onClick={(event) => {
+                      event.preventDefault();
+                      deleteCourse(course._id);
+                    }} className="btn btn-danger float-end"
+                      id="wd-delete-course-click">
+                      Delete
+                    </button>
                   </div>
                 </div>
               </Link>
