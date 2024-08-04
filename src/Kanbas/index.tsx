d9c827a6b43b845d862d0cfeb40e8005404c0d69
zhanweicao <chanweicao@gmail.com>
2024-08-04 14:00:44 -0700

    4.2 DONE

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,24 +3,32 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
-import * as db from "./Database";
-import { useState } from "react";
+// import * as db from "./Database";
+import * as client from "./Courses/client";
+import { useEffect, useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 
 export default function Kanbas() {
-    const [courses, setCourses] = useState<any[]>(db.courses);
+    const [courses, setCourses] = useState<any[]>([]);
+    useEffect(() => {
+        fetchCourses();
+    }, []);
+
     const [course, setCourse] = useState<any>({
         _id: "1234", name: "New Course", number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
     });
-    const addNewCourse = () => {
-        setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+    const addNewCourse = async () => {
+        const newCourse = await client.createCourse(course);
+        setCourses([...courses, newCourse]);
     };
-    const deleteCourse = (courseId: any) => {
-        setCourses(courses.filter((course) => course._id !== courseId));
+    const deleteCourse = async (courseId: any) => {
+        await client.deleteCourse(courseId);
+        setCourses(courses.filter((c) => c._id !== courseId));
     };
-    const updateCourse = () => {
+    const updateCourse = async () => {
+        await client.updateCourse(course);
         setCourses(
             courses.map((c) => {
                 if (c._id === course._id) {
@@ -31,6 +39,14 @@ export default function Kanbas() {
             })
         );
     };
+    const fetchCourses = async () => {
+        const courses = await client.fetchAllCourses();
+        setCourses(courses);
+    };
+    useEffect(() => {
+        fetchCourses();
+    }, []);
+
 
     return (
         <Provider store={store}>
