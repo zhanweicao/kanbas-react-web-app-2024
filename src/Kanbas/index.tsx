94113b7a3ca2bcdae509dbfae97c0bcb214099d5
zhanweicao <chanweicao@gmail.com>
2024-08-07 15:51:01 -0700

    5.8 DONE

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import Account from "./Account";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
@@ -57,14 +58,14 @@ export default function Kanbas() {
                     <Routes>
                         <Route path="/" element={<Navigate to="Dashboard" />} />
                         <Route path="/Account/*" element={<Account />} />
-                        <Route path="Dashboard" element={<Dashboard
+                        <Route path="Dashboard" element={<ProtectedRoute><Dashboard
                             courses={courses}
                             course={course}
                             setCourse={setCourse}
                             addNewCourse={addNewCourse}
                             deleteCourse={deleteCourse}
-                            updateCourse={updateCourse} />} />
-                        <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+                            updateCourse={updateCourse} /></ProtectedRoute>} />
+                        <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
                         <Route path="Calendar" element={<h1>Calendar</h1>} />
                         <Route path="Inbox" element={<h1>Inbox</h1>} />
                     </Routes>
