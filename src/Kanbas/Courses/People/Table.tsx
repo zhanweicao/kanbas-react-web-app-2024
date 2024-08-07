48b8bc4c6633566a5721a46f1d8b27f6b23f9b64
zhanweicao <chanweicao@gmail.com>
2024-08-07 00:12:34 -0700

    3.6.5 DONE

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -37,7 +37,7 @@ export default function PeopleTable() {
     }, []);
     return (
         <div id="wd-people-table">
-            <PeopleDetails />
+            <PeopleDetails fetchUsers = {fetchUsers}/>
             <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                 className="form-control float-start w-25 me-2 wd-filter-by-name" />
             <select value={role} onChange={(e) => filterUsersByRole(e.target.value)}
